4d5f54fb3533ff17024bf60a0aeca1e41177c1f5
Thomas Barton <thomasryanbarton@gmail.com>
2013-04-24 13:12:02 -0700

    cleaned project dir

--- master/bottomMobile.php
+++
@@ -1,9 +0,0 @@
-		</div>
-		<div data-role="footer">
-			<img id="quote" src="../images/quote.png" alt="quote" />
-		</div>
-		<br>
-		<footer><a href="redirect.php" rel="external">Full Site</a> | <a href="userLogout.php">Logout</a></footer>
-	</div>
-</body>
-</html>
--- test.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo strtotime("now"), "\n";
-echo strtotime("08/11/1988"), "\n";
-?>
